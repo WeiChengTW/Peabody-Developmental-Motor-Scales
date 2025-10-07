02c60f8e821cafd6d4cec9ef4a6323f482e54139
William <chang93928@gmail.com>
2025-10-07 15:34:54 +0800

    1007

--- summer_vacation/Diamond-multi.v5i.multiclass/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Diamond-multi > 2025-08-01 2:34pm
-https://universe.roboflow.com/uuriglass/diamond-multi-xziij
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- summer_vacation/Is_cross/Is_cross/label_num.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-labels = pd.read_csv('labels.csv')  # 或讀你label檔案
-print(labels['asymmetric'].sum(), labels['nonstraight'].sum(), labels['nonrightangle'].sum())
